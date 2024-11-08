84f4722759b5a670a4f146694c9445b8bb01d448
luohuixi <2388287244@qq.com>
2024-11-08 20:40:35 +0800

    第三周作业

--- 交叉打印.go
+++ 交叉打印.go
@@ -5,35 +5,31 @@ import (
 )
 func main(){
 	a:=0
-	var lock1,lock2,lock3,lock4 sync.Mutex
+	var lock1,lock2 sync.Mutex
 	var wait sync.WaitGroup
 	ch:=make(chan string)
 	English:="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	number:="0123456789"
 	wait.Add(1)
-	lock4.Lock()
+	lock2.Lock()
 	go func(){	
         for k,value:=range English{
 			lock1.Lock()
-			lock3.Lock()
 			ch<-string(value)
-			if (k%2==1&&a==0) {lock4.Unlock()} else {lock3.Unlock()}
-			lock1.Unlock()
+			if (k%2==1&&a==0) {lock2.Unlock()} else {lock1.Unlock()}
 		}
 		wait.Done()
 	}()
 	go func(){
         for k,value:=range number{
 			lock2.Lock()
-			lock4.Lock()
 			ch<-string(value)
 			if k==9 {
 				a=1
-				lock3.Unlock()
+				lock1.Unlock()
 				return
 			}
-			if k%2==1 {lock3.Unlock()} else {lock4.Unlock()}
-			lock2.Unlock()
+			if k%2==1 {lock1.Unlock()} else {lock2.Unlock()}
 		}
 	}()
 	go func(){
